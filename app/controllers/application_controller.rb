79b20144e7870c286f1279d758a6f16db6cd6db6
tetorapod-a <65341661+tetorapod-a@users.noreply.github.com>
2020-06-17 16:32:46 +0900

    デプロイ準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  before_action :basic_auth, if: :production?
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
+    end
+  end
+# 以下を追記
+  def production?
+    Rails.env.production?
+  end
+
+
+
 end
