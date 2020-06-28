a8269350eeb288d4886f569569140e60d8dd6c47
yuki0623 <hil.olas.c222@gmail.com>
2020-06-28 18:12:43 +0900

    販売価格の編集
    販売手数料、利益の追加

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -46,3 +46,20 @@ $(document).on('turbolinks:load', ()=> {
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
 });
+
+$(function(){
+  $('#price_calc').on('input', function(){
+    var data = $('#price_calc').val(); 
+    var profit = Math.round(data * 0.9)
+    var fee = (data - profit)
+    $('.right_bar').html(fee)
+    $('.right_bar').prepend('¥')
+    $('.right_bar_2').html(profit)
+    $('.right_bar_2').prepend('¥')
+    $('#price').val(profit)
+    if(profit == '') {
+    $('.right_bar_2').html('');
+    $('.right_bar').html('');
+    }
+  })
+})
