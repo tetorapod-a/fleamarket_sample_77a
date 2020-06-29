8f7ce1eb1bc29269982eee8d661924fe6c1184ad
oecchi <65586213+oecchi@users.noreply.github.com>
2020-06-29 16:29:29 +0900

    修正06291630

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,11 @@
 class UsersController < ApplicationController
 
-
   def edit
   end
-
 
   def show
     @user = User.find(current_user.id)
     @address = Address.find(current_user.id)
-    # unless @address.valid?
-    #   flash.now[:alert] = @address.errors.full_messages
-    #   render :new_address and return
-    # end
-    # @user.build_address(@address.attributes)
-    # @user.save
-    # session["devise.regist_data"]["user"].clear
-    # sign_in(:user, @user)
   end  
 
-    # def configure_sign_up_params
-    #   devise_parameter_sanitizer.permit(:sign_up, keys:
-    #   [:nickname,  :birthday, :first_name, :last_name])
-    # end
-
-    # def address_params
-    #   params.require(:address).permit(:first_name,, :last_name, :first_name_kana,
-    #   :last_name_kana, :postal_code, :prefecture, :prefecture_id,  :city, :municipality, 
-    #   :address, :apartment, :phone)
-    # end
-
-  # private
-
-  #   def address_params
-  #     params.require(:address).permit(:first_name, :first_name_kana, :last_name, :last_name_kana, :postal_code,
-  #                                     :prefectur, :city, :house_number, :apartment, :phone)
-  #   end
 end
