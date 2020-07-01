ecd35830491c3da1bc4b18ca6f45e7dedfae1bfc
yuki0623 <hil.olas.c222@gmail.com>
2020-07-01 16:41:27 +0900

    修正

--- db/migrate/20200701062840_create_likes.rb
+++ db/migrate/20200701062840_create_likes.rb
@@ -1,8 +1,9 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
-      t.references :user,        null: false, foreign_key: true
-      t.references :item,        null: false, foreign_key: true
+      t.references :user,               null: false, foreign_key: true
+      t.references :item,               null: false, foreign_key: true
+      t.index     [:user_id, :item_id], unique: true  
       t.timestamps
     end
   end
