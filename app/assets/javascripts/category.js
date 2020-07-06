4f7d70d4655ba531da517fc3afc427a7a207b293
oecchi <65586213+oecchi@users.noreply.github.com>
2020-07-06 14:10:27 +0900

    出品の修正は可能

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -49,10 +49,10 @@ $(function(){
         dataType: 'json'
       })
       .done(function(children){
-        $('#children_wrapper').remove(); //親が変更された時、子以下を削除する
-        $('#grandchildren_wrapper').remove();
-        $('#size_wrapper').remove();
-        $('#brand_wrapper').remove();
+        // $('#children_wrapper').remove(); //親が変更された時、子以下を削除する
+        $('#child_category').remove();
+        $('#grandchild_categoryr').remove();
+        // $('#brand_wrapper').remove();
         var insertHTML = '';
         children.forEach(function(child){
           insertHTML += appendOption(child);
@@ -63,8 +63,8 @@ $(function(){
         alert('カテゴリー取得に失敗しました');
       })
     }else{
-      $('#children_wrapper').remove(); //親カテゴリーが初期値になった時、子以下を削除する
-      $('#grandchildren_wrapper').remove();
+      $('#child_categoryr').remove(); //親カテゴリーが初期値になった時、子以下を削除する
+      $('#grandchild_category').remove();
       $('#size_wrapper').remove();
       $('#brand_wrapper').remove();
     }
@@ -72,7 +72,6 @@ $(function(){
   // 子カテゴリー選択後のイベント
   $(document).on('change', '#child_category', function(){
     var childId = $('#child_category option:selected').data('category'); //選択された子カテゴリーのidを取得
-    console.log(childId);
     if (childId != "-----"){ //子カテゴリーが初期値でないことを確認
       $.ajax({
         url: '/items/get_category_grandchildren',
@@ -83,9 +82,9 @@ $(function(){
       .done(function(grandchildren){
         console.log(grandchildren)
         if (grandchildren.length != 0) {
-          $('#grandchildren_wrapper').remove(); //子が変更された時、孫以下を削除するする
-          $('#size_wrapper').remove();
-          $('#brand_wrapper').remove();
+          $('#grandchild_category').remove(); //子が変更された時、孫以下を削除する
+          // $('#size_wrapper').remove();
+          // $('#brand_wrapper').remove();
           var insertHTML = '';
           grandchildren.forEach(function(grandchild){
             insertHTML += appendOption(grandchild);
@@ -97,9 +96,9 @@ $(function(){
         alert('カテゴリー取得に失敗しました');
       })
     }else{
-      $('#grandchildren_wrapper').remove(); //子カテゴリーが初期値になった時、孫以下を削除する
-      $('#size_wrapper').remove();
-      $('#brand_wrapper').remove();
+      $('#grandchild_category').remove(); //子カテゴリーが初期値になった時、孫以下を削除する
+      // $('#size_wrapper').remove();
+      // $('#brand_wrapper').remove();
     }
   });
 });
