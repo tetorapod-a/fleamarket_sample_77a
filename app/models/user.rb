a1f6ee8eaeccc8b07307f3318fd6ead34b515b05
oribanda <oribanda2@gmail.com>
2020-07-08 19:41:26 +0900

    エラーメッセージ修正途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :nickname, :email, :first_name, :first_name_kana, :last_name, :last_name_kana, :birthday, presence: true
   validates_format_of :password, :with => /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, :message => "は７文字以上の英数混在で入力してください"
   validates_format_of :first_name, :last_name, :with => /\A[ぁ-んァ-ン一-龥]/, :message => "は全角で入力してください"
